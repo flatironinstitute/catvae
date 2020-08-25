fixing dimensionality issues with covariance matrix

catvae/distributions/likelihood.py
@@ -1,5 +1,5 @@
 import torch
-from torch.distributions import MultivariateNormal
+from torch.distributions import MultivariateNormal, Multinomial
 from catvae.distributions.mvn import MultivariateNormalFactorSum
 
 
@@ -12,7 +12,7 @@ def K(x):
             torch.sum(torch.lgamma(1 + x), dim=-1))
 
 
-def expectation_mvn_factor_sum_multinomial(
+def expectation_mvn_factor_sum_multinomial_taylor(
         q: MultivariateNormalFactorSum, psi: torch.Tensor, x: torch.Tensor,
         gamma: torch.Tensor):
     r""" Lower bound for th efirst expectation involving
@@ -40,6 +40,34 @@ def expectation_mvn_factor_sum_multinomial(
     exp_ln = torch.exp(psi @ mu_eta + 0.5 * cov_eta)
     # approximation for normalization factor
     denom = (1 / gamma) * exp_ln.sum(axis=-1) + torch.log(gamma) - 1
+    return K(x) + x @ (logits - denom)
+
+
+def expectation_mvn_factor_sum_multinomial_bound(
+        q: MultivariateNormalFactorSum, psi: torch.Tensor, x: torch.Tensor):
+    r""" Lower bound for th efirst expectation involving
+         multinomial reconstruction error
+
+    Parameters
+    ----------
+    q : MultivariateNormalFactorSum
+       q(eta | x) = N(W V(h(x)), WDW^T + \frac{1}{nf} Psi^T diag(x)^{-1} Psi)
+    psi : torch.Tensor
+       ILR basis of dimension D x (D - 1)
+    x : torch.Tensor
+       Input counts of dimension N x D
+    gamma : torch.Tensor
+       Auxiliary variational parameter to be jointly optimized.
+
+    Notes
+    -----
+    We can't get closed-form updates here, so we need to obtain another
+    lower bound for this expectation.  See Silva et al 2017 for more details.
+    """
+
+    mu_eta = q.mean
+    logits = psi @ mu_eta
+    denom = torch.logexpsum(logits, sum=-1)
     return K(x) + x @ (logits - denom)
 
 

catvae/distributions/mvn.py
@@ -157,13 +157,33 @@ class MultivariateNormalFactorSum(Distribution):
 
     @property
     def covariance_matrix(self):
-        sigmaU1 = (1 / self.n) * self.U1 @ torch.diag(self.S1) @ self.U1.t()
+        if len(self.S1.shape) == 1:
+            P = torch.diag(self.S1)
+        elif len(self.S1.shape) == 2:
+            P = torch.stack([
+                torch.diag(self.S1[i, :].squeeze())
+                for i in range(self.S1.shape[0])
+            ], dim=0)
+        else:
+            raise ValueError(f'Cannot handle dimensions {self.S1.shape}')
+
+        sigmaU1 = (1 / self.n) * self.U1 @ P @ self.U1.t()
         sigmaU2 = self.U2 @ torch.diag(self.S2) @ self.U2.t()
         return sigmaU1 + sigmaU2
 
     @property
     def precision_matrix(self):
-        invS1 = self.n * self.U1 @ torch.diag(1 / self.S1) @ self.U1.t()
+        if len(self.S1.shape) == 1:
+            invP = torch.diag(1 / self.S1)
+        elif len(self.S1.shape) == 2:
+            invP = torch.stack([
+                torch.diag(1 / self.S1[i, :].squeeze())
+                for i in range(self.S1.shape[0])
+            ], dim=0)
+        else:
+            raise ValueError(f'Cannot handle dimensions {self.S1.shape}')
+
+        invS1 = self.n * self.U1 @ inv @ self.U1.t()
         W = self.U2
         invD = torch.diag(1 / self.S2)
 

catvae/distributions/tests/test_mvn.py
@@ -100,7 +100,7 @@ class TestMultivariateNormalFactorSum(unittest.TestCase):
         loc = torch.zeros(d-1)
         dist = MultivariateNormalFactorSum(loc, psi, 1 / p, W, D, n)
         cov = dist.covariance_matrix
-        self.assertEqual(cov.shape, (self.d - 1, self.d - 1))
+        self.assertEqual(cov.shape, (n, d - 1, d - 1))
 
     def test_precision_matrix(self):
         # tests how accurately the inverse covariance matrix can be computed

catvae/models/linear_cat_vae.py
@@ -4,7 +4,7 @@ from gneiss.cluster import random_linkage
 from gneiss.balances import _balance_basis
 from catvae.composition import closure, ilr
 from catvae.distributions.likelihood import (
-    expectation_mvn_factor_sum_multinomial,
+    expectation_mvn_factor_sum_multinomial_bound,
     expectation_joint_mvn_factor_mvn_factor_sum,
 )
 import numpy as np
@@ -105,9 +105,8 @@ class LinearCatVAE(nn.Module):
         qdist = MultivariateNormalFactorSum(
             eta, self.psi, 1 / p,
             W, D, n)
-        return expectation_mvn_factor_sum_multinomial(
-            qdist, self.psi, x, self.gamma
-        )
+        return expectation_mvn_factor_sum_multinomial_bound(
+            qdist, self.psi, x)
 
     def multinomial_kl(self, x, eta, z):
         """ KL divergence between asymptotic multinomial and decoding normal