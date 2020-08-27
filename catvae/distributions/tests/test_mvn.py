9cc24bbe5d9a51045b8b5a875139f4a2af8bd4f6
mortonjt <jamietmorton@gmail.com>
2020-08-26 22:59:14 -0600

    tests passing for new mvn distribution

--- catvae/distributions/tests/test_mvn.py
+++ catvae/distributions/tests/test_mvn.py
@@ -1,6 +1,7 @@
 import unittest
 from catvae.distributions.mvn import MultivariateNormalFactor
 from catvae.distributions.mvn import MultivariateNormalFactorSum
+from catvae.distributions.mvn import MultivariateNormalFactorIdentity
 from torch.distributions import MultivariateNormal
 from catvae.distributions.utils import seed_all
 import torch
@@ -165,6 +166,75 @@ class TestMultivariateNormalFactorSum(unittest.TestCase):
 
         self.assertAlmostEqual(
             float(qeta.entropy()), float(qtest.entropy()), places=0)
+
+
+class TestMultivariateNormalFactorIdentity(unittest.TestCase):
+    def setUp(self):
+        n = 200
+        d = 100
+        k = 4
+        seed_all(0)
+        self.W = torch.randn((d, k))
+        self.D = torch.rand(k)
+        self.n = n
+        self.d = d
+        self.s2 = torch.Tensor([3])
+        self.Id = torch.eye(self.d)
+
+    def test_covariance_matrix(self):
+        loc = torch.zeros(self.d)
+        exp = (self.W @ torch.diag(self.D) @ self.W.t() +
+               self.s2 * self.Id)
+        dist = MultivariateNormalFactorIdentity(
+            loc, self.s2, self.D, self.W)
+        cov = dist.covariance_matrix
+        self.assertEqual(cov.shape, (self.d, self.d))
+
+    def test_precision_matrix(self):
+        # tests how accurately the inverse covariance matrix can be computed
+        loc = torch.zeros(self.d)
+        dist = MultivariateNormalFactorIdentity(
+            loc, self.s2, self.D, self.W)
+        exp = torch.inverse(self.W @ torch.diag(self.D) @ self.W.t() +
+                            self.s2 * self.Id)
+        tt.assert_allclose(exp, dist.precision_matrix,
+                           rtol=1, atol=1 / (math.sqrt(self.d)))
+
+    def test_log_det(self):
+        loc = torch.zeros(self.d)
+        dist = MultivariateNormalFactorIdentity(
+            loc, self.s2, self.D, self.W)
+        cov = dist.covariance_matrix
+        res = dist.log_det
+        exp = torch.slogdet(cov)[1]
+        tt.assert_allclose(res, exp)
+
+    def test_rsample(self):
+        loc = torch.ones(self.d)
+        dist = MultivariateNormalFactorIdentity(
+            loc, self.s2, self.D, self.W)
+        samples = dist.rsample([10000])
+        self.assertAlmostEqual(float(samples.mean()), 1, places=2)
+
+    def test_log_prob(self):
+        loc = torch.ones(self.d)
+
+        wdw = self.W @ torch.diag(self.D) @ self.W.t()
+        sI = self.s2 * self.Id
+        sigma = sI + wdw
+        dist2 = MultivariateNormal(loc, covariance_matrix=sigma)
+        samples = dist2.rsample([10000])
+        exp_logp = dist2.log_prob(samples)
+
+
+        dist1 = MultivariateNormalFactorIdentity(
+            loc, self.s2, self.D, self.W)
+        res_logp = dist1.log_prob(samples)
+
+        self.assertAlmostEqual(float(exp_logp.mean()), float(res_logp.mean()), places=3)
+
+    def test_entropy(self):
+        pass
 
 
 if __name__ == '__main__':
