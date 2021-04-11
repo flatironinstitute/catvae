fixing units on batch-prior

catvae/models/linear_vae.py
@@ -180,7 +180,8 @@ class LinearBatchVAE(LinearVAE):
         self.ilr_dim = input_dim - 1
         posPsi = torch.sparse.FloatTensor(
             self.Psi.indices(), torch.abs(self.Psi.values()))
-        batch_prior = ilr(batch_prior, posPsi)
+        batch_prior = torch.log((posPsi @ batch_prior.T).T)
+        print(torch.min(batch_prior), torch.max(batch_prior))
         self.register_buffer('batch_prior', batch_prior)
         self.batch_logvars = nn.Parameter(torch.zeros(self.ilr_dim))
         # self.input_embed = nn.Parameter(

catvae/trainer.py
@@ -367,11 +367,14 @@ class LightningBatchLinearVAE(LightningVAE):
         # we'll read in the metadata / table twice, whatever
         # We may need to adjust this in the future.
         table = load_table(self.hparams.train_biom)
-        batch_prior = pd.read_table(self.hparams.batch_prior, dtype=str)
+        batch_prior = pd.read_table(self.hparams.batch_prior, dtype=str, header=None)
         batch_prior = batch_prior.set_index(batch_prior.columns[0])
-        batch_prior = batch_prior.loc[table.ids(axis='observation')]
-        # TODO: impute with 1 for now, will need to think about this
-        batch_prior = batch_prior.fillna(1)
+        batch_prior = batch_prior.reindex(table.ids(axis='observation'))
+        batch_prior = batch_prior.fillna(5) # fill with 5 for missing values
+        if table.shape[0] != len(batch_prior):
+            raise ValueError(f'Features ({table.shape[0]}) in table do not match'
+                             f'features {len(batch_prior)} in batch prior')
+        #batch_prior = batch_prior.fillna(5)  # default for missing labels
         batch_prior = batch_prior.values.astype(np.float64).reshape(1, -1)
         self.batch_prior = torch.Tensor(batch_prior).float()
         self.metadata = pd.read_table(