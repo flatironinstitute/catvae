1625a8b00f95603544a410c9a2d2308edfc1084b
mortonjt <jamietmorton@gmail.com>
2021-05-19 11:15:44 -0600

    flak8e linear_cat_vae

--- catvae/models/linear_cat_vae.py
+++ catvae/models/linear_cat_vae.py
@@ -4,33 +4,34 @@ from gneiss.cluster import random_linkage
 from gneiss.balances import sparse_balance_basis
 from scipy.sparse import coo_matrix
 import numpy as np
-from torch.distributions import Multinomial, MultivariateNormal, Normal
-from catvae.composition import closure, ilr
+from torch.distributions import Multinomial, Normal
+from catvae.composition import ilr
 from catvae.distributions.mvn import MultivariateNormalFactorIdentity
 from typing import Callable
-
+import warnings
 LOG_2_PI = np.log(2.0 * np.pi)
 
 
 class LinearCatVAE(nn.Module):
-
-    def __init__(self, input_dim : int, hidden_dim : int,
-                 init_scale : float = 0.001,
-                 basis : coo_matrix = None,
-                 encoder_depth : int = 1,
-                 imputer : Callable[[torch.Tensor], torch.Tensor]=None,
-                 batch_size : int =10, bias=True):
+    def __init__(self, input_dim: int, hidden_dim: int,
+                 init_scale: float = 0.001,
+                 basis: coo_matrix = None,
+                 encoder_depth: int = 1,
+                 imputer: Callable[[torch.Tensor], torch.Tensor] = None,
+                 batch_size: int = 10, bias: bool = True):
         super(LinearCatVAE, self).__init__()
+        warnings.warn('LinearCatVAE is going to be deprecated in a '
+                      'future version of this package', DeprecationWarning)
         self.initialize(input_dim, hidden_dim, init_scale,
                         basis, encoder_depth, imputer,
                         batch_size, bias)
 
-    def initialize(self, input_dim : int, hidden_dim : int,
-                   init_scale : float = 0.001,
-                   basis : coo_matrix = None,
-                   encoder_depth : int = 1,
-                   imputer : Callable[[torch.Tensor], torch.Tensor]=None,
-                   batch_size : int =10, bias=True):
+    def initialize(self, input_dim: int, hidden_dim: int,
+                   init_scale: float = 0.001,
+                   basis: coo_matrix = None,
+                   encoder_depth: int = 1,
+                   imputer: Callable[[torch.Tensor], torch.Tensor] = None,
+                   batch_size: int = 10, bias: bool = True):
         self.hidden_dim = hidden_dim
         self.bias = bias
         # Psi must be dimension D - 1 x D
@@ -67,7 +68,8 @@ class LinearCatVAE(nn.Module):
                     encoder_layer.weight.data.normal_(0.0, init_scale)
 
         else:
-            self.encoder = nn.Linear(self.input_dim, hidden_dim, bias=self.bias)
+            self.encoder = nn.Linear(self.input_dim, hidden_dim,
+                                     bias=self.bias)
             self.encoder.weight.data.normal_(0.0, init_scale)
 
         self.decoder = nn.Linear(hidden_dim, self.input_dim, bias=False)
@@ -75,7 +77,6 @@ class LinearCatVAE(nn.Module):
         self.log_sigma_sq = nn.Parameter(torch.tensor(0.01))
         self.eta = nn.Parameter(torch.zeros(batch_size, self.input_dim))
         self.eta.data.normal_(0.0, init_scale)
-        #self.decoder.weight.data.normal_(0.0, init_scale)
         zI = torch.ones(self.hidden_dim).to(self.eta.device)
         zm = torch.zeros(self.hidden_dim).to(self.eta.device)
         self.register_buffer('Psi', Psi)
@@ -101,8 +102,6 @@ class LinearCatVAE(nn.Module):
         logit_loss = qdist.log_prob(self.eta).mean()
         mult_loss = Multinomial(logits=logp.t()).log_prob(x).mean()
         loglike = mult_loss + logit_loss + prior_loss
-
-        #print(x.mean(), z_mean.mean(), loglike)
         return -loglike
 
     def reset(self, x):
@@ -119,12 +118,12 @@ class LinearCatVAE(nn.Module):
 
 
 class LinearBatchCatVAE(LinearCatVAE):
-    def __init__(self, input_dim : int, hidden_dim : int,
-                 init_scale : float = 0.001,
-                 basis : coo_matrix = None,
-                 encoder_depth : int = 1,
-                 imputer : Callable[[torch.Tensor], torch.Tensor]=None,
-                 batch_size : int =10, bias=True):
+    def __init__(self, input_dim: int, hidden_dim: int,
+                 init_scale: float = 0.001,
+                 basis: coo_matrix = None,
+                 encoder_depth: int = 1,
+                 imputer: Callable[[torch.Tensor], torch.Tensor] = None,
+                 batch_size: int = 10, bias: bool = True):
         super(LinearBatchCatVAE, self).__init__(
             input_dim, hidden_dim, init_scale,
             basis, encoder_depth, imputer,
@@ -132,11 +131,7 @@ class LinearBatchCatVAE(LinearCatVAE):
         )
 
     def encode(self, x):
-        #B = B.sum(axis=0) + 1
-        #B = B.unsqueeze(0)
-        #batch_effects = (self.Psi @ B.t()).t()
         hx = ilr(self.imputer(x), self.Psi)
-        #hx -= batch_effects  # Subtract out batch effects
         z = self.encoder(hx)
         return z
 
